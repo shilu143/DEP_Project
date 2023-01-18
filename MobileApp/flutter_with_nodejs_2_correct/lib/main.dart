25b3a067f4b4635533afe8f53b5ee6d88b29febf
nishantgta <2020csb1103@iitrpr.ac.in>
2023-01-19 00:25:49 +0530

    Added mailing functionality for app

--- MobileApp/flutter_with_nodejs_2_correct/lib/main.dart
+++ MobileApp/flutter_with_nodejs_2_correct/lib/main.dart
@@ -27,28 +27,29 @@ class _MyHomePageState extends State<MyHomePage> {
 
   // This function will send the message to our backend.
   void sendMessage(msg) {
-  IOWebSocketChannel? channel;
-  // We use a try - catch statement, because the connection might fail.
-  try {
-    // Connect to our backend.
-    channel = IOWebSocketChannel.connect('ws://localhost:3000');
-  } catch (e) {
-    // If there is any error that might be because you need to use another connection.
-    print("Error on connecting to websocket: " + e.toString());
-  }
-  // Send message to backend
-  channel?.sink.add(msg);
-
-  // Listen for any message from backend
-  channel?.stream.listen((event) {
-    // Just making sure it is not empty
-    if (event!.isNotEmpty) {
-      print(event);
-      // Now only close the connection and we are done here!
-      channel!.sink.close();
+    IOWebSocketChannel? channel;
+    // We use a try - catch statement, because the connection might fail.
+    try {
+      // Connect to our backend.
+      channel = IOWebSocketChannel.connect('ws://localhost:3000');
+      print(channel.toString());
+    } catch (e) {
+      // If there is any error that might be because you need to use another connection.
+      print("Error on connecting to websocket: " + e.toString());
     }
-  });
-}
+    // Send message to backend
+    channel?.sink.add(msg);
+
+    // Listen for any message from backend
+    channel?.stream.listen((event) {
+      // Just making sure it is not empty
+      if (event!.isNotEmpty) {
+        print(event);
+        // Now only close the connection and we are done here!
+        channel!.sink.close();
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
--- MobileApp/flutter_with_nodejs_2_correct/lib/server/index.js
+++ MobileApp/flutter_with_nodejs_2_correct/lib/server/index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const { Server } = require('ws');
 const sendMail = require("./controller/sendMail");
 
+require("dotenv").config();
+
+
 const PORT = process.env.PORT || 3000; //port for https
 
 /* const server = express().use((req, res) => res.send("Hi there")).listen(PORT, () => console.log(`Listening on ${PORT}`)); */
@@ -10,17 +13,18 @@ const server1 = express();
 
 .get('/email',sendMail); */
 
-const server = express();
-server.use((req, res) => res.send("Hi there"))
+const server = express().use("/",(req, res) =>{ 
+    console.log("nishant"); res.send("Hi there");})
 .listen(PORT, () => console.log(`Listening on ${PORT}`));
-
-
 
 const wss = new Server({ server });
 
 wss.on('connection', function(ws, req) {
     ws.on('message', message => {
         var dataString = message.toString();
-        console.log(dataString)
+        console.log("shilu");
+        console.log(dataString);
+        /* ws.send("ashish"); */
+        sendMail(dataString);
     })
 });
