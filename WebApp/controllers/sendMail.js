e894eb31c68c875176aa0d8ba44877ca3556c78a
Ashish Alok <2020csb1076@iitrpr.ac.in>
2023-01-24 00:03:40 +0530

    Otp Verification stage -2

--- WebApp/controllers/sendMail.js
+++ WebApp/controllers/sendMail.js
@@ -5,25 +5,55 @@ const userModel = require('../models/userModel');
 const otpModel = require('../models/otpModel');
 const saltRounds = 10;
 
-const sendMail = async (req, res,next) => {
+const sendMail = async (req, res, next) => {
     let targetEmail = req.body.username;
     let targetRole = 0;
     let newHash;
     let OTP = Math.floor(10000 + Math.random() * 90000); //5 digit OTP
     const saltRounds = await bcrypt.genSalt(10);
-    await bcrypt.hash(String(OTP), saltRounds).then(function(hash) {
+    await bcrypt.hash(String(OTP), saltRounds).then(function (hash) {
         newHash = hash;
     });
-    
-    
+
+
+    // const newEntry = new otpModel({
+    //     userName: targetEmail,
+    //     otp: newHash
+    // });
+
+    // await newEntry.save();
+    // await otpModel.findOneAndUpdate({userName:targetEmail}, {otp:newHash}, {upsert: true}, function(err, doc) {
+    //     if (err) return res.send(500, {error: err});
+
+    //     // return res.send('Succesfully saved.');
+    // });
+    let isAvailable = null;
+    await otpModel.find({ userName: targetEmail }).then(result => {
+        isAvailable = result;
+        
+    });
+    // console.log(isAvailable);
+
     const newEntry = new otpModel({
         userName: targetEmail,
         otp: newHash
     });
+    if (isAvailable.length == 0 ) {
+      
+        console.log("data unavailable");
+        await newEntry.save();
+    }
+    else
+    {
+        console.log("data available");
+        await otpModel.findOneAndDelete({userName:targetEmail})
+        .then( () => {
+            newEntry.save();
+        }) 
 
-    await newEntry.save();
+    }
 
-    // await otpModel.findOneAndUpdate({userName:targetEmail},{userName:targetEmail,otp:newHash});
+
 
     const msg = {
         from: "Email Verification",
@@ -32,7 +62,7 @@ const sendMail = async (req, res,next) => {
         text: `Use ${OTP} as One Time Password (OTP) to log in to your account. This OTP is valid for 5 minutes.${'\n\n'}Please do not share this OTP with anyone for security reasons.`
     };
 
-  // connect with the smtp  
+    // connect with the smtp  
     let transporter = await nodemailer.createTransport({
         service: 'gmail',
         auth: {
@@ -43,8 +73,8 @@ const sendMail = async (req, res,next) => {
         host: 'smtp.gmail.com'
     });
 
-  let info = await transporter.sendMail(msg, err => {
-        if(err) {
+    let info = await transporter.sendMail(msg, err => {
+        if (err) {
             console.log("Something went wrong, ", err);
             res.send("<h1>Something went wrong :(</h1>");
         }
@@ -52,9 +82,9 @@ const sendMail = async (req, res,next) => {
             console.log("Message sent successfully");
             res.send("<h1>Message sent successfully :)</h1>");
         }
-  });
-  res.cookie('userName',targetEmail);
-  res.redirect('/login/otp');
+    });
+    res.cookie('userName', targetEmail);
+    res.redirect('/login/otp');
 
 };
 
